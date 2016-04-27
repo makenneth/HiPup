8baf641fe04a419100bc94a636c87aecc4e28bc4
makenneth <kenneth.km.ma@gmail.com>
2016-04-27 08:24:09 -0700

    Fixed a line

--- app/models/GroupParticipant.rb
+++ app/models/GroupParticipant.rb
@@ -2,7 +2,7 @@ class GroupParticipant < ActiveRecord::Base
 		validates :group_id, :participant_id, presence: true
 		validates :group_id, uniqueness: {scope: :participant_id}
 
-		belongs_to :group
+		belongs_to :group, primary_key: :group_id, class_name: :Group
 		belongs_to :participant, primary_key: :id,
 								foreign_key: :participant_id, class_name: :User
 end
