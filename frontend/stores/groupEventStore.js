3ef010a8b207e6fb8f614303b3c3b10a59174d4e
makenneth <kenneth.km.ma@gmail.com>
2016-04-28 11:45:05 -0700

    updated link to heroku site

--- frontend/stores/groupEventStore.js
+++ frontend/stores/groupEventStore.js
@@ -1,3 +1,24 @@
 var Store = require('flux/utils').Store,
 		AppDispatcher = require('../dispatcher/dispatcher'),
-		GroupEventStore = new Store(AppDispatcher);
+		GroupEventStore = new Store(AppDispatcher),
+		GroupEventConstants = require('../constants/groupEventConstants');
+
+var _groupEvents = {};
+
+var _resetGroupEvents = function(groupEvents){
+	groupEvents.forEach(function(groupEvent){
+		_groupEvents[groupEvent.id] = groupEvent;
+	});
+};
+
+var _resetSinglesEvent = function(singleEvent){
+	
+};
+
+GroupEventStore.all = function(){
+	return _groupEvents;
+};
+
+GroupEventStore.__onDispatch = function(payload){
+
+};
