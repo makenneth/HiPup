f1658187bf4cc4f42b766cf498d289aa1c488390
makenneth <kenneth.km.ma@gmail.com>
2016-04-27 19:49:04 -0700

    Fixed database issues

--- db/migrate/20160427173408_change_username_to_ownername.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUsernameToOwnername < ActiveRecord::Migration
-  def change
-  	rename_column :users, :username, :owner_name
-  end
-end
--- db/migrate/20160427174251_add_username_to_user.rb
+++ db/migrate/20160427174251_add_username_to_user.rb
@@ -1,5 +1,5 @@
 class AddUsernameToUser < ActiveRecord::Migration
   def change
-    add_column :users, :username, :string, null: false
+    add_column :users, :owner_name, :string
   end
 end
