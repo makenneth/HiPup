2e32b58580b829dfcf19148ed65467203f50c1ea
makenneth <kenneth.km.ma@gmail.com>
2016-05-04 08:55:00 -0700

    Edited file path

--- frontend/components/group/editGroupForm.jsx
+++ frontend/components/group/editGroupForm.jsx
@@ -5,7 +5,7 @@ var React = require('react'),
 		GroupStores = require('../../stores/groupStore'),
 		Autocomplete = require('../../mixin/autoComplete'),
 		UserStore = require('../../stores/userStore'),
-		GroupFormMixin = require('../../mixin/GroupFormMixin'),
+		GroupFormMixin = require('../../mixin/groupFormMixin'),
 		ErrorStore = require('../../stores/errorStore');
 var GroupEditForm = React.createClass({
 	mixins: [CurrentUserState, Autocomplete, GroupFormMixin],
