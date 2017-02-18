b83cb73a5bf04688d9d200602b62fa786ebaa1b5
makenneth <kenneth.km.ma@gmail.com>
2017-02-18 13:07:30 -0800

    Moved reset and app scss to assets folder

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,23 @@ module HipUp
     config.action_dispatch.default_headers = {
         'X-Frame-Options' => 'SAMEORIGIN'
     }
+
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins 'localhost:4000'
+
+        resource '/cors',
+          :headers => :any,
+          :methods => [:post],
+          :credentials => true,
+          :max_age => 0
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
     config.cache_store = :memory_store
     config.active_record.raise_in_transactional_callbacks = true
   end
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
-import reducer from "./modules/reducer";
-import middleware from "./middleware";
+import { createStore, applyMiddleware, compose } from 'redux';
+import createLogger from 'redux-logger';
+import middleware from './middleware';
 
-const createStoreWithMiddleware = applyMiddleware(middleware)(createStore);
-const store = createStoreWithMiddleware(reducer);
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(middleware, logger)(createStore);
 
-export default store;
+export default function createFinalStore() {
+  const reducer = require('./modules/reducer');
+  const store = createStoreWithMiddleware(reducer);
+
+  if (module.hot) {
+    module.hot.accept('./modules/reducer', () =>
+      store.replaceReducer(require('./modules/reducer'))
+    );
+  }
+
+  return store;
+}
