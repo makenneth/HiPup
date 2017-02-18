befda1e8dcbb852192b3bc72d105963e32588bda
makenneth <kenneth.km.ma@gmail.com>
2017-02-18 12:41:22 -0800

    Fixed hot module replacement

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { ReduxAsyncConnect } from 'redux-async-connect';
-import store from './redux/store';
+import createStore from './redux/store';
 import getRoutes from './routes';
 import { updateLocation } from 'redux/modules/geolocation';
 
 if (process.env.NODE_ENV !== 'production') {
   window.React = React;
 }
+
+const store = createStore();
 
 document.addEventListener('DOMContentLoaded', () => {
   updateLocation(store);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname,
-  entry: ['./src/app.js'],
+  context: path.resolve(__dirname, 'src'),
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:4000',
+    path.resolve(__dirname, 'src', 'app.js')
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    colors: true,
+    stats: 'errors-only',
+    contentBase: './static',
+    port: 4000
+  },
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: './static',
+    publicPath: 'http://localhost:4000/',
     filename: 'bundle.js'
   },
   module: {
@@ -12,21 +28,22 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      }
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      { test: /\.json$/, loader: 'json-loader' },
     ]
   },
   resolve: {
-    modulesDirectories: [
-      'src',
-      'node_modules'
-    ],
+    modulesDirectories: ['src', 'node_modules'],
     mainFiles: ['index'],
     extensions: ['', '.js', '.jsx']
   },
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 };
