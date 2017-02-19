1a45d06ccc5edfbaba8e1cfcf206481f4ea2106c
makenneth <kenneth.km.ma@gmail.com>
2017-02-18 17:20:36 -0800

    Added state transformer and improved console.log for immutable js;

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,19 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import createLogger from 'redux-logger';
 import middleware from './middleware';
 
-const logger = createLogger();
+const logger = createLogger({
+  stateTransformer: (state) => {
+    return state && Object.keys(state).reduce((h, r) => {
+      if (r === 'reduxAsyncConnect') {
+        h[r] = state[r];
+      } else {
+        h[r] = state[r].toJS();
+      }
+      return h;
+    }, {});
+  }
+});
+
 const createStoreWithMiddleware = applyMiddleware(middleware, logger)(createStore);
 
 export default function createFinalStore() {
